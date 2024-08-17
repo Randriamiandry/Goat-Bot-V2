c3d1d49c101ef2bc39f79be35b4b0688fd10e964
Randriamiandry <randriamiandryathanasios@gmail.com>
2024-08-17 14:02:59 +0300

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -51,7 +51,7 @@ module.exports = {
           const names = categories[category].commands.sort();
           for (let i = 0; i < names.length; i += 3) {
             const cmds = names.slice(i, i + 3).map((item) => `🏷️${item}`);
-            msg += `\n│ ${cmds.join(" ".repeat(Math.max(1 - cmds.join("").length)))}`;
+            msg += `\n│ ${cmds.join(" ".repeat(Math.max(1, 10- cmds.join("").length)))}`;
           }
 
           msg += `\n╰────────ꔪ`;
